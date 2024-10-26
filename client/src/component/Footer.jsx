9f15f41a482ab108ce86406fd770783e7166a928
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-10-26 16:06:56 -0500

    added navigation bar and footer

---
+++ client/src/component/Footer.jsx
@@ -0,0 +1,32 @@
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+export default function Footer() {
+    return (
+        <AppBar position="static" className="nav-bar" sx={{ backgroundColor:'white'}}>
+            <Toolbar className="tool-bar">
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography className="logo-title" component="a" href="/" variant="h6" sx={{  textDecoration: 'none', color: 'black', paddingLeft: '10px'}}>
+                        FinTechstic
+                    </Typography>
+                </div>
+                <div className="footer-right">
+                    <div className="legal-stuff">
+                        <Typography sx={{ fontWeight: 600, color: 'black', padding:'10px'}}>Privacy Policy</Typography>
+                        <Typography sx={{ fontWeight: 600, color: 'black', padding:'10px'}}>Term of Services</Typography>
+                    </div>
+                    <div className="social-media">
+                        <LinkedInIcon className="icon-black"></LinkedInIcon>
+                        <YouTubeIcon className="icon-black"></YouTubeIcon>
+                        <InstagramIcon className="icon-black"></InstagramIcon>
+                    </div>
+                </div>
+                
+                
+            </Toolbar>
+        </AppBar>
+    );
+}
---
+++ client/src/component/NavigationBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+const NavigationBar = () => {
+  return (
+    <AppBar position="static" className="nav-bar" sx={{ backgroundColor:'white'}}>
+            <Toolbar className="tool-bar">
+                <div style={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
+                    <Typography className="logo-title" component="a" href="/" variant="h6" sx={{  textDecoration: 'none', color: 'black', paddingLeft: '10px', fontWeight: 'bold'}}>
+                        FinTechstic
+                    </Typography>
+                </div>
+                <div>
+                    <Button color="inherit" href="/sign-up" className="button-white" sx={{ mr: 2, backgroundColor: 'white', color: 'black', fontWeight: 600, borderRadius: '10px', padding: '5px 15px 5px 15px', marginLeft: '10px', border: '1px solid black', '&:hover': {backgroundColor: '#e2e2e2',}, }}>Sign Up</Button>
+                    <Button color="inherit" href="/sign-in" className="button-blue" sx={{ mr: 2, backgroundColor: '#2E46CD', color: 'white', fontWeight: 600, borderRadius: '10px', padding: '5px 15px 5px 15px', marginLeft: '10px', border: '1px solid #2E46CD', '&:hover': {backgroundColor: '#1565C0',}, }}>Login</Button>
+                </div>
+                
+            </Toolbar>
+        </AppBar>
+  )
+}
+
+export default NavigationBar
