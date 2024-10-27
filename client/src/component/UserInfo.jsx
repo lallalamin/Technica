9ee61f29ff3b123a23ed5f296eddc22f94c6bdb8
Anhakian <anhnguyenngochoang2003@gmail.com>
2024-10-27 10:53:14 +0000

    show email on authenticated users

--- client/src/component/UserInfo.jsx
+++ client/src/component/UserInfo.jsx
@@ -5,11 +5,12 @@ import { Typography, Button } from "@mui/material";
 const UserInfo = withAuthInfo((props) => {
     const logoutFunction = useLogoutFunction();
     const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
+    console.log("props:", props);
 
     if (props.isLoggedIn) {
         return (
             <div>
-                <Typography sx={{ textDecoration: 'none', color: 'black' }}>Welcome, {props.email}</Typography>
+                <Typography sx={{ textDecoration: 'none', color: 'black' }}>Welcome, {props.user.email}</Typography>
                 <Button onClick={() => redirectToAccountPage()} color="inherit" className="button-white" sx={{ mr: 2, backgroundColor: 'white', color: 'black', fontWeight: 600, borderRadius: '10px', padding: '5px 15px 5px 15px', marginLeft: '10px', border: '1px solid black', '&:hover': { backgroundColor: '#e2e2e2' } }}>Account</Button>
                 <Button onClick={() => logoutFunction(true)} color="inherit" className="button-blue" sx={{ mr: 2, backgroundColor: '#2E46CD', color: 'white', fontWeight: 600, borderRadius: '10px', padding: '5px 15px 5px 15px', marginLeft: '10px', border: '1px solid #2E46CD', '&:hover': { backgroundColor: '#1565C0' } }}>Logout</Button>
             </div>
